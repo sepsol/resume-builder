404369380be0ab7b0f11f1ab121aee6a85fcc460
sepsol <sepsol.works@gmail.com>
2024-02-15 15:58:00 -0800

    Applied the new vite plugin to apply tsconfig paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 import react from '@vitejs/plugin-react'
 import stylex from 'vite-plugin-stylex';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), stylex()],
+  plugins: [tsconfigPaths(), react(), stylex()],
 })
