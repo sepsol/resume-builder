0aaad373c18ba1533f3a384384a06b4f3954c094
sepsol <sepsol.works@gmail.com>
2024-02-15 16:13:50 -0800

    Removed as much stuff as possible from boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
 import { useState } from 'react'
-import reactLogo from 'src/assets/react.svg'
-import viteLogo from '/vite.svg'
-import 'src/App.css'
 import * as stylex from '@stylexjs/stylex'
 
 function App() {
@@ -9,14 +6,6 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1 {...stylex.props(styles.red)}>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -26,9 +15,6 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
