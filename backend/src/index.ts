d9268c813f1dbfbf434e307cb37acaef1ec27845
sepsol <sepsol.works@gmail.com>
2024-02-20 15:58:01 -0800

    Created an `express` server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello, world!");
+import express from 'express';
+
+const app = express();
+const port = 4000;
+
+app.get('/', (req, res) => {
+  res.send("Hello, world!");
+});
+
+app.listen(port, () => {
+  return console.log(`Express is listening at http://localhost:${port}`);
+});
